3d08d377f6ac2e9249bb1e9222021bc272371f46
jihun <yjhn0410@gmail.com>
2025-08-02 12:50:59 +0900

    feat: register

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const indexRouter = require('./routes/index');
 const app = express();
 
 require('dotenv').config();
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+app.use('/api',indexRouter);
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose.connect(mongoURI,{useNewUrlParser:true})
   .then(()=>console.log('mongoose connected'))
---
+++ controller/user.controller.js
@@ -0,0 +1,23 @@
+const User = require("../model/User");
+const bcrypt = require('bcryptjs');
+
+const userController = {};
+
+userController.createUser = async(req,res) => {
+    try{
+        let {email,name,password,level} = req.body;
+        const user = await User.findOne({email});
+        if (user) {
+            throw new Error('이미 가입된 유저입니다');
+        }
+        const salt = await bcrypt.genSaltSync(10);
+        password = await bcrypt.hash(password,salt);
+        const newUser = new User({email,password,name,level:level?level:'customer'});
+        await newUser.save();
+        return res.status(200).json({status:'success'})
+    } catch(error){
+        res.status(400).json({status:'fail',error:error.message});
+    }
+}
+
+module.exports=userController;
