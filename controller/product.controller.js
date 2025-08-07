bc57a14e9defeffc7c25b0b73587fe7b11d87216
jihun <yjhn0410@gmail.com>
2025-08-07 16:28:18 +0900

    feat: 상품 검색하기

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -24,11 +24,21 @@ productController.createProduct = async(req,res) => {
 
 productController.getProducts = async(req,res) => {
     try{
-        const products = await Product.find({});
-        res.status(200).json({status:'success', data: products});
+        const {page,name} = req.query;
+        // if (name) {
+        //     const products = await Product.find({name:{$regex:name,$options:'i'}}) // i: 대소문자 구분 x
+        // } else {
+        //     const products = await Product.find({});
+        // }
+        const cond = name ? {name: {$regex:name,$options:'i'}} : {}
+        let query = Product.find(cond);
+
+        const productList = await query.exec();
+        res.status(200).json({status:'success', data: productList});
     } catch(error){
         res.status(400).json({status:'fail',error:error.message});
     }
 };
+
 
 module.exports = productController;
