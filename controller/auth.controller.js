1524145e86f5f02834478f3769f817b6efadf36e
jihun <yjhn0410@gmail.com>
2025-08-15 15:25:37 +0900

    feat: google login

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,7 +3,9 @@ const User = require("../model/User");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-const authController = {}
+const authController = {};
+const {OAuth2Client} = require('google-auth-library');
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 
 authController.loginWithEmail = async(req, res) => {
@@ -51,6 +53,34 @@ authController.checkAdminPermission = async(req,res,next) => {
         next();
     } catch(error){
         res.status(400).json({status:'fail', error:error.message});
+    }
+}
+
+authController.loginWithGoogle = async(req,res) => {
+    try{
+        const {token} = req.body;
+        const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+        const ticket = await googleClient.verifyIdToken({
+            idToken: token,
+            audience: GOOGLE_CLIENT_ID,
+        });
+        const {email,name} = ticket.getPayload();
+        let user = await User.findOne({email});
+        if (!user) {
+            const randomPassword = ''+Math.floor(Math.random()*100000000);
+            const salt = await bcrypt.genSalt(10);
+            const newPassword = await bcrypt.hash(randomPassword, salt);
+            user = new User({
+                name,
+                email,
+                password: newPassword
+            });
+            await user.save();
+        }
+        const sessionToken = await user.generateToken();
+        res.status(200).json({status:'success', user, token: sessionToken});
+    } catch(error){
+        res.status(400).json({status:'fail',error:error.message});
     }
 }
 
--- routes/auth.api.js
+++ routes/auth.api.js
@@ -4,4 +4,6 @@ const authController = require('../controller/auth.controller');
 
 router.post('/login',authController.loginWithEmail);
 
+router.post('/google', authController.loginWithGoogle);
+
 module.exports = router;
